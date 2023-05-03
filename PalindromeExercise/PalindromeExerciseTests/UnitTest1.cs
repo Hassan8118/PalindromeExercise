f7cef7a12870d7b92734c11866500e8fdd5fc0cd
hassan8118 <mujtabahassan1234567@gmail.com>
2023-05-03 14:35:08 -0400

    PalindromeExDone

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,26 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("wow", true)]
+        [InlineData("rotator", true)]
+        [InlineData("deed", true)]
+        [InlineData("dog", false)]
 
+        public void IsAPalindromeTest(string word, bool expected)
+        {
+            var ws = new WordSmith();
+
+            var actual = ws.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
+
+
+
+
+
+
+        
     }
 }
